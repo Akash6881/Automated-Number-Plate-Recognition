4ac5dd116f7c10a2b779b961322e83a001a02ca8
Akash <akashdas6881@gmail.com>
2021-10-28 17:58:07 +0530

    detection working cropped images left

--- rest-server.py
+++ rest-server.py
@@ -1,16 +1,43 @@
 from wsgiref import simple_server
-from flask import Flask, Request, Response, render_template
+from flask import Flask, request, Response, render_template
 from flask_cors import CORS,cross_origin
+from predict_number_plate import DetectNumberPlate
+
+
 application = Flask(__name__)
 
 CORS(application)
+
+
+inputFileName = "image1.jpeg"
+imagePath = "images/" + inputFileName
+
+class model:
+    def __init__(self):
+        self.path_to_frozen_graph = 'model_files/frozen_inference_graph.pb'
+        self.path_to_label = 'model_files/classes.pbtxt'
+        print("Akash Das")
+        self.predict_obj = DetectNumberPlate(self.path_to_frozen_graph, self.path_to_label)
+        print("Akash Das 22")
+
 
 @application.route("/", methods = ["GET"])
 @cross_origin()
 def home():
     return render_template('index.html')
 
+
+@application.route("/predict", methods = ["POST"])
+@cross_origin()
+def predict():
+    inpImage = request.json["image"]
+
+
 if __name__ == '__main__':
+    name = "a"
+    model_obj = model()
+    model_obj.predict_obj.predict(imagePath)
+
     host = "127.0.0.1"
     port = 5000
     httpd = simple_server.make_server(host,port, application)
